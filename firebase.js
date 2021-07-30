63ce373ec6c22869f3ae4d29f7ce0cd9563bb2fa
Jashwanth Bheemapaka <65611955+jashwanth999@users.noreply.github.com>
2021-07-30 15:12:09 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,7 @@ import '@firebase/storage';
 import '@firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2Z2vP12wV3-x1lX_ir56dhxNxAyyY-2g",
-  authDomain: "whatsapp-48c08.firebaseapp.com",
-  projectId: "whatsapp-48c08",
-  storageBucket: "whatsapp-48c08.appspot.com",
-  messagingSenderId: "1064813625541",
-  appId: "1:1064813625541:web:76565ecd82dc34388f70ac",
-  measurementId: "G-DLCE48D0S2"
+ 
 };
 
 if (!firebase.apps.length) {
